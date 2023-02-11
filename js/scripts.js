95b451105624211411959cf8a5df71f5ae6f7f59
Joseph Murray <josephwmurray@gmail.com>
2023-02-10 18:58:24 -0800

    hide and display error message upon button click

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,18 @@ window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideError();
+    const displayArray = [];
     const inputNumber = parseInt(document.querySelector("input#inputNumber").value);
-    
-  };
-
-};
+    if (inputNumber != Number) {
+      document.getElementById("error-message").removeAttribute("class");
+    } else {
+      for (i = 0; i < inputNumber.length; i++) {
+        console.log(i);
+        let arrayNum = inputNumber(i);
+        displayArray.push(arrayNum);
+        console.log(arrayNum);
+      };
+    };
+  console.log(displayArray);
+  }
+}
